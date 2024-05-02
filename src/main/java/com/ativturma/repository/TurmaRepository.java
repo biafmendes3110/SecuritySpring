e54ddec4a71f63ff2ba76a2651eaa5441cfc4aed
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-05-02 15:44:41 -0300

    Security

---
+++ src/main/java/com/ativturma/repository/TurmaRepository.java
@@ -0,0 +1,14 @@
+package com.ativturma.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ativturma.entities.Turma;
+
+public interface TurmaRepository extends JpaRepository <Turma, Long>{
+	List<Turma> findByNome(String nome);
+	List<Turma> findByDescricao(String descricao);
+	List<Turma> findByNomeAndDescricao(String nome, String descricao);
+
+}
---
+++ src/test/java/com/ativturma/AtivTurma2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativturma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivTurma2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
